b4b55160e5a6a650424e5d10f45e09f0ca479fe5
Marcus Oshodi <147557402+moshodifimio@users.noreply.github.com>
2024-04-09 17:31:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 from fastapi import FastAPI
+import os
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -10,3 +11,8 @@ app = FastAPI()
 async def root():
     logging.info("Hello World endpoint was called")
     return {"message": "Hello World"}
+
+@app.get("/env_var_message")
+async def root():
+    logging.info("Custom endpoint was called")
+    return {"message": f"{os.getenv('MESSAGE')}"}
